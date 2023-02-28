905beb81004a0313efd2aee16f0aed038b69533f
howardtang96 <howardtang96@gmail.com>
2023-03-01 01:32:34 +0800

    main

--- forms/contact.php
+++ forms/contact.php
@@ -18,13 +18,14 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
+  $contact->to = 'info@muchachotrading.com';
+  $contact->from_name = $_POST['sender name'];
+  $contact->from_email = $_POST['sender email'];
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
+
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
@@ -32,6 +33,12 @@
     'port' => '587'
   );
   */
+
+  $contact->smtp = array(
+    'host' => 'muchacotrading.com',
+    'username' => 'info@muchachotrading.com',
+    'password' => 'Aa19960419wah!',
+    'port' => '587'
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
